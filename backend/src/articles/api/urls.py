a066ab6dd2e966b1ee8dd95898f5d2e2dd205905
Kylema <kylemaa95@gmail.com>
2019-07-11 21:20:41 -0700

    router for api sub finished

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -4,5 +4,5 @@ from .views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
     path('', ArticleListView.as_view()),
-    path('pk', ArticleDetailView.as_view()),
+    path('<pk>', ArticleDetailView.as_view()),
 ]
--- backend/src/djreact/settings.py
+++ backend/src/djreact/settings.py
@@ -50,8 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'djreact.urls'
